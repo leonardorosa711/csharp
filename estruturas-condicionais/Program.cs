fe70ec69607f44634b7f51ef1544a33759c3d917
matheushenrikL3 <marcelineschiavon@gmail.com>
2024-11-06 17:28:21 -0400

    adicionado estrutura condicional if

--- estruturas-condicionais/Program.cs
+++ estruturas-condicionais/Program.cs
@@ -12,3 +12,47 @@ else
 {
     Console.WriteLine($"Média {media:F2}: Aprovado");
 }
+
+// Estruturas Condicionais 
+double nota4 = 5;
+double nota5 = 8.0;
+double nota6 = 5.0;
+double mediaNota = (nota4 + nota5 + nota6) / 3;
+if (mediaNota >= 7)
+{
+    Console.WriteLine($"Média {media:F2}: Aprovado");
+}
+else if (mediaNota >= && mediaNota < 7)
+{
+    Console.WriteLine($"Média {media:F2}: Recuperação");
+}
+else
+{
+    Console.WriteLine($"Média {media:F2}: Reprovado");
+}
+Console.WriteLine("Digite uma vogal");
+string? letra = Console.ReadLine();
+if (letra == "a" || letra == "A")
+{
+    Console.WriteLine($"Você digitou a vogal: {letra}");
+}
+else if(letra == "e" || letra == "E")
+{
+    Console.WriteLine($"Você digitou a vogal: {letra}");
+}
+else if(letra == "i" || letra == "I")
+{
+    Console.WriteLine($"Você digitou a vogal: {letra}");
+}
+else if(letra == "o" || letra == "O")
+{
+    Console.WriteLine($"Você digitou a vogal: {letra}");
+}
+else if(letra == "u" || letra == "U")
+{
+    Console.WriteLine($"Você digitou a vogal: {letra}");
+}
+else
+{
+    Console.WriteLine($"A letra {letra} não é uma vogal.");
+}
